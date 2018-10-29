2ffb49c997f20208c22a696dc31be44b17041ab7
Nick Thomas <nichoth@gmail.com>
2018-10-29 02:51:47 -0700

    more stuff

--- index.js
+++ index.js
@@ -1,20 +1,41 @@
 var restify = require('restify')
-var { plugins } = require('restify')
-var errs = requrie('restify-errors')
+var { plugins } = restify
+var errs = require('restify-errors')
+var Pino = require('restify-pino-logger')
+var Cors = require('restify-cors-middleware')
+
+var pino = Pino()
+var cors = Cors({
+    origins: ['*'],
+    allowHeaders: ['X-Requested-With']
+})
 
 var server = restify.createServer()
-
 server
+    .pre(cors.preflight)
+    .use(cors.actual)
+    .use(pino)
+    .use()
     .use(plugins.queryParser({ mapParams: false }))
     .use(plugins.bodyParser())
 
 server.get('/hello/:name', function (req, res, next) {
     res.send('hello ' + req.params.name)
+    req.log.info('hello route')
     next()
 })
 
 server.listen(8000, function () {
-    console.log(server.name + ' listening at ' + server.url)
+    pino.logger.info(server.name + ' listening at ' + server.url)
+})
+
+server.on('close', function () {
+    pino.logger.info(`${server.name} at ${server.url} close`)
+})
+
+server.on('error', function (err) {
+    pino.logger.fatal(err)
+    throw err
 })
 
 module.exports = server
